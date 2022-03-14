5c654ad4fbf217a2a86fa740e187e7730b011b5a
GitHub Action <action@github.com>
2022-03-14 12:59:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/whatskd/plugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/whatskd/plugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.whatskd.plugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.whatskd.plugindemo.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/whatskd/plugindemo/MyPluginTest.kt
+++ src/test/kotlin/com/github/whatskd/plugindemo/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.whatskd.plugindemo
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
